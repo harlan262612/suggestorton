90d5ae30b66f1b4199c0bf191d7f64b8c152f6ea
harlan <harlan@bitoex.com>
2021-03-02 18:14:38 +0800

    add a vote table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_095130) do
+ActiveRecord::Schema.define(version: 2021_03_02_101339) do
 
   create_table "topics", force: :cascade do |t|
     t.string "tittle"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "topic_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
